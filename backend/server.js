d311c4b82654a59b8e86e50571f7dbfd4414ce1f
Giselle <gisellelsz@gmail.com>
2021-08-02 01:51:39 +0200

    ApiRouter and body-parser

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var apiRouter = require('./apiRouter').router;
 
 var server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 server.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send('Server ready!');
 });
+
+server.use('/api/', apiRouter);
 
 server.listen(8080, function () {
     console.log('Server listening');

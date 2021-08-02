29e03cee6df26fb08c438f484499817ae2be09ae
Giselle <gisellelsz@gmail.com>
2021-08-02 22:33:13 +0200

    Add user login

--- backend/routes/usersCtrl.js
+++ backend/routes/usersCtrl.js
@@ -1,5 +1,5 @@
 var bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
+var jwtUtils = require('../utils/jwt.utils');
 var models = require('../models');
 
 //Routes
@@ -49,6 +49,35 @@ module.exports = {
         });
     },
     login: function(req, res) {
+        //Params
+        var email = req.body.email;
+        var password = req.body.password;
 
+        if (email == null || password == null) {
+            return res.status(400).json({ 'error': 'missing parameters' });
+        }
+        //Verify mail regex, password, etc.
+        models.User.findOne({
+            where: { email: email }
+        })
+        .then(function(userFound) {
+            if (userFound) {
+                bcrypt.compare(password, userFound.password, function(errBycrypt, resBycrypt) {
+                    if(resBycrypt) {
+                        return res.status(200).json({
+                            'userId': userFound.id,
+                            'token': jwtUtils.generateTokenForUser(userFound)
+                        });
+                    } else {
+                        return res.status(403).json({ 'error': 'invalid password' });
+                    }
+                });
+            } else {
+                return res.status(404).json({ 'error': 'user not exist in DB'});
+            }
+        })
+        .catch(function(err) {
+            return res.status(500).json({ 'error': 'unable to verify user' });
+        });
     }
 }

0aec56e2fc563e8aa73d552a97bd1c37661c0889
Giselle <gisellelsz@gmail.com>
2021-08-03 22:04:31 +0200

    Add waterfall register and login

--- backend/routes/usersCtrl.js
+++ backend/routes/usersCtrl.js
@@ -1,7 +1,7 @@
 var bcrypt = require('bcrypt');
 var jwtUtils = require('../utils/jwt.utils');
 var models = require('../models');
-
+var asyncLib = require('async');
 
 //Constants
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -33,38 +33,59 @@ module.exports = {
             return res.status(400).json({ 'error': 'password invalid (must length 4 - 8 and include 1 number at least)' });
         }
 
+        
         //Verify mail regex, password, etc.
-        models.User.findOne({
-            attributes: ['email'],
-            where: { email: email }
-        })
-        .then(function(userFound) {
-            if (!userFound) {
-                bcrypt.hash(password, 5, function( err, bcryptedPassword ) {
-                    var newUser = models.User.create({
-                        email: email,
-                        username: username,
-                        password: bcryptedPassword,
-                        bio: bio,
-                        isAdmin:0
-                    })
-                    .then(function(newUser) {
-                        return res.status(201).json({
-                            'userId': newUser.id
-                        })
-                    })
-                    .catch(function(err) {
-                        return res.status(500).json({ 'error': 'cannot add user' });
-                    });
+
+        asyncLib.waterfall([
+            function(done) {
+                models.User.findOne({
+                    attributes: ['email'],
+                    where: { email: email }
+                })
+                .then(function(userFound) {
+                    done(null, userFound);
+                })
+                .catch(function(err) {
+                    return res.status(500).json({ 'error': 'unable to verify user' });
                 });
+            },
+            function(userFound, done) {
+                if (!userFound) {
+                    bcrypt.hash(password, 5, function( err, bcryptedPassword ) {
+                        done(null, userFound, bcryptedPassword);
+                    });
             } else {
                 return res.status(409).json({ 'error': 'user already exist' });
             }
-        })
-        .catch(function(err) {
-            return res.status(500).json({ 'error': 'unable to verify user' });  
-        });
-    },
+        },
+        function(userFound, bcryptedPassword, done) {
+            var newUser = models.User.create({
+                email: email,
+                username: username,
+                password: bcryptedPassword,
+                bio: bio,
+                isAdmin:0
+            })
+            .then(function(newUser) {
+                done(newUser);
+            })
+            .catch(function(err) {
+                return res.status(500).json({ 'error': 'cannot add user' });
+            });
+        }
+    ], function(newUser) {
+        if (newUser) {
+            return res.status(201).json({
+                'userId': newUser.id
+            });
+        } else {
+            return res.status(500).json({ 'error': 'cannot add user' });
+        }
+    });
+},
+
+    //Verify mail regex, password, etc.
+
     login: function(req, res) {
         //Params
         var email = req.body.email;
@@ -73,28 +94,45 @@ module.exports = {
         if (email == null || password == null) {
             return res.status(400).json({ 'error': 'missing parameters' });
         }
-        //Verify mail regex, password, etc.
-        models.User.findOne({
-            where: { email: email }
-        })
-        .then(function(userFound) {
+
+        asyncLib.waterfall([
+          function(done) {
+            models.User.findOne({
+                where: { email: email }
+            })
+            .then(function(userFound) {
+                done(null, userFound);
+            })
+            .catch(function(err) {
+                return res.status(500).json({ 'error': 'unable to verify user' });
+            });
+          },
+          function(userFound, done) {
             if (userFound) {
                 bcrypt.compare(password, userFound.password, function(errBycrypt, resBycrypt) {
-                    if(resBycrypt) {
-                        return res.status(200).json({
-                            'userId': userFound.id,
-                            'token': jwtUtils.generateTokenForUser(userFound)
-                        });
-                    } else {
-                        return res.status(403).json({ 'error': 'invalid password' });
-                    }
+                    done(null, userFound, resBycrypt);
                 });
+          } else {
+            return res.status(404).json({ 'error': 'user not exist in DB'});
+          }
+        },
+        function(userFound, resBycrypt, done) {
+            if(resBycrypt) {
+                done(userFound);
             } else {
-                return res.status(404).json({ 'error': 'user not exist in DB'});
+                return res.status(403).json({ 'error': 'invalid password' });
             }
-        })
-        .catch(function(err) {
-            return res.status(500).json({ 'error': 'unable to verify user' });
-        });
-    }
+        }
+    ], function(userFound) {
+        if (userFound) {
+            return res.status(200).json({
+                'userId': userFound.id,
+                'token': jwtUtils.generateTokenForUser(userFound)
+            });
+        } else {
+            return res.status(500).json({ 'error': 'cannot log on user' });
+        }
+    });
+},
+
 }

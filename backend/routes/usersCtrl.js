e44762773516b498acfa0963d509325388ac0f13
Giselle <gisellelsz@gmail.com>
2021-08-03 03:19:23 +0200

    Regex email and pass

--- backend/routes/usersCtrl.js
+++ backend/routes/usersCtrl.js
@@ -2,6 +2,11 @@ var bcrypt = require('bcrypt');
 var jwtUtils = require('../utils/jwt.utils');
 var models = require('../models');
 
+
+//Constants
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
+
 //Routes
 module.exports = {
     register: function(req, res) {
@@ -14,6 +19,18 @@ module.exports = {
 
         if(email == null || username == null || password == null) {
             return res.status(400).json({ 'error': 'missing parameters' });
+        }
+
+        if(username.length >= 13 || username.length <= 4) {
+            return res.status(400).json({ 'error': 'wrong username (must be length 5 - 12)' }); 
+        }
+
+        if(!EMAIL_REGEX.test(email)) {
+            return res.status(400).json({ 'error': 'email is not valid' });
+        }
+
+        if(!PASSWORD_REGEX.test(password)) {
+            return res.status(400).json({ 'error': 'password invalid (must length 4 - 8 and include 1 number at least)' });
         }
 
         //Verify mail regex, password, etc.

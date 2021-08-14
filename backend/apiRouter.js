1608ab9e5f5c9aaa6da757ce274f1fc0c048deb8
Giselle <gisellelsz@gmail.com>
2021-08-14 03:39:58 +0200

    Add message ctrl and routes

--- backend/apiRouter.js
+++ backend/apiRouter.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var usersCtrl = require('./routes/usersCtrl');
+var messagesCtrl = require('./routes/messagesCtrl');
 
 //Router
 exports.router = (function() {
@@ -10,6 +11,10 @@ exports.router = (function() {
     apiRouter.route('/users/login/').post(usersCtrl.login);
     apiRouter.route('/users/me/').get(usersCtrl.getUserProfile);
     apiRouter.route('/users/me/').put(usersCtrl.updateUserProfile);
+
+    //Messages routes
+    apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
+    apiRouter.route('/messages/').get(messagesCtrl.listMessages);
 
     return apiRouter;
 })();

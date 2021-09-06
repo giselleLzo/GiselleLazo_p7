7ee56d8870c44f8831d8309e808ce52c356bdfeb
Giselle <gisellelsz@gmail.com>
2021-09-07 00:07:47 +0200

    Add home

---
+++ frontend/groupomania/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="home">
+       <Nav />
+       <Post />
+       <Footer />
+    </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue';
+import Post from '@/components/Post.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+    created(){
+        this.$store.dispatch('getUsers');
+        this.$store.dispatch('getUserInfos');
+        this.$store.dispatch('getPostsInfos');
+        this.$store.dispatch('getComments');
+    },
+    name: 'Home',
+    components: {
+        Nav,
+        Post,
+        Footer,
+    },
+};
+</script>

ec1026bf20b20d452d6cd57ec3f1d56244bce58c
Giselle <gisellelsz@gmail.com>
2021-09-07 00:05:54 +0200

    Add nav and footer components

---
+++ frontend/groupomania/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="bg-light fixed-bt">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <p class="ft-font text-center mt-3">Groupomania © 2021</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'Footer',
+}
+</script>
---
+++ frontend/groupomania/src/components/Nav.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a href="#">
+          <div class="navbar-brand brand-light"
+            ><img
+              src="@/assets/logo.png"
+              alt="Logo Groupomania"
+              width="250"
+              height="60"
+          /></div>
+        </a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+            <li class="nav-item" @click="switchToProfile()">
+              <a class="nav-link border-top">Profile</a>
+            </li>
+            <li class="nav-item" @click="logout()"> 
+              <a class="nav-link border-top">Déconnexion</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      user: 'userInfos',
+    }),
+  },
+  methods: {
+    logout: function () {
+      this.$store.commit('logout');
+      this.$router.push('/');
+    },
+    switchToProfile: function () {
+      this.$router.push('/profile')
+    }
+  }
+}
+</script>

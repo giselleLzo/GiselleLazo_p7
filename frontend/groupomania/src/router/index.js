ccbb9494bd6a4542820695ebc883a21087c6c3ba
Giselle <gisellelsz@gmail.com>
2021-10-02 14:19:24 +0200

    Chages routes

--- frontend/groupomania/src/router/index.js
+++ frontend/groupomania/src/router/index.js
@@ -1,84 +1,28 @@
-import { createRouter, createWebHistory} from 'vue-router';
-import Login from '@/views/Login.vue';
-import Home from '@/views/Home.vue';
-import Profile from '@/views/Profile.vue';
-import Posts from '@/components/Posts.vue';
-import ProfileDelete from '@/components/ProfileDelete.vue';
-import store from '../store/index'
+import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [
+import Home from '../views/Home.vue'
+
+const routes =[
     {
-        name: 'login',
-        path: '/',
-        component: Login,
-        meta: {
-            title: 'Connexion',
-            requiresUser: true,
-        },
-    }, {
-        name: 'home',
-        path: '/home',
-        component: Home,
-        meta: {
-            title: 'Bienvenue',
-            requiresAuth: true,
-        },
-    }, {
-        name: 'profile',
-        path: '/profile',
-        component: Profile,
-        props: true,
-        meta: {
-            title: 'Profile',
-            requiresAuth: true,
-        },
-    }, {
-        name: 'posts',
-        path: '/posts/:id',
-        component: Posts,
-        props: true,
-        meta: {
-            title: 'Post',
-            requiresAuth: true,
-        },
-    }, {
-        name: 'profileDelete',
-        path: '/profileDelete',
-        component: ProfileDelete,
-        props: true,
-    }, 
-];
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('../views/Signup.vue')
+  },
+  {
+    path: '/',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+]
 
 const router = createRouter({
     history: createWebHistory(),
-    routes,
+    routes 
 })
 
-router.beforeEach((to, from, next) => {
-    if (to.matched.some((routes) => routes.meta.requiresAuth)) {
-        if (!store.state.user.token) {
-            next({
-                name: 'login'
-            });
-        }else {
-            next();
-        }
-    }else if (to.matched.some((routes) => routes.meta.requiresUser)) {
-        if ((store.state.user.token)) {
-            next({
-                name: 'home' || 'posts' || 'profile',
-            });
-        }else {
-            next();
-        }
-    }else {
-        next();
-    }
-});
-
-router.afterEach((to, from) => {
-    console.log(from, to);
-    document.title = to.meta.title;
-})
-
-export default router;
+export default router
